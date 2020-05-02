063d527fb627be0f8e4db0b6faecb9ddf69a9e83
Daniel Gatto <d.mario.gatto@gmail.com>
2020-05-02 18:29:00 +0930

    embedded font

--- src/MuGet.Forms/Resources/DarkTheme.xaml.cs
+++ src/MuGet.Forms/Resources/DarkTheme.xaml.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Internals;
-using Xamarin.Forms.Xaml;
 
 namespace MuGet.Forms.Styles
 {
